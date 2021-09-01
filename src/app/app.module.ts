862ee4ea40f7a1b7b67039d34cc2a8ffaa7f4671
Anand Galla <gallaanand24@gmail.com>
2021-09-01 14:22:17 -0500

    added nav bar section to the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { OurServicesComponent } from './components/our-services/our-services.component';
+
+//import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    OurServicesComponent
   ],
   imports: [
     BrowserModule,
